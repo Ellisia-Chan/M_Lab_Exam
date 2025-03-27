a82e37cf5c05e77d139a900d47a5efd9a3de2878
Ellisia <christianjudevillaber@gmail.com>
2025-03-27 23:07:55 +0800

    coin anim and sprite

---
+++ Assets/scripts/Coin.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    private const string COIN_COLLECT = "coinCollect";
+
+    private BoxCollider2D boxCollider2D;
+    private Animator animator;
+
+    private void Awake() {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<PlayerController>() != null) {
+            boxCollider2D.enabled = false;
+            animator.SetTrigger(COIN_COLLECT);
+        }
+    }
+
+    private void DestroyCoin() {
+        Destroy(gameObject);
+    }
+
+}
